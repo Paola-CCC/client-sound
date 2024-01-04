3d961711f66dac66e9c226c0e2f463d158a15029
Paola_C <paola.cyprien@hetic.net>
2024-01-04 02:46:49 +0100

    test clean

--- src/hooks/axiosFetch.js
+++ src/hooks/axiosFetch.js
@@ -1,63 +1,63 @@
 import { useEffect, useCallback } from "react";
 import { useAPIContext } from "../contexts/APIContextProvider";
 
-
 const useAxiosFetchCourse = () => {
+  const { courseAPI } = useAPIContext();
 
-    const { courseAPI } = useAPIContext();
+  const fetchData = useCallback(
+    async (value) => {
+      try {
+        if (value !== undefined) {
+          const response = await courseAPI.searching(value);
+          return response;
+        }
+      } catch (error) {
+        console.error("Error fetching course data:", error);
+      }
+    },
+    [courseAPI]
+  );
 
-    const fetchData = useCallback(async (value) => {
-        try {
-              if( value !== undefined) {
-                const response = await courseAPI.searching(value);
-                return response;
-              }
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-          } catch (error) {
-            console.error("Error fetching course data:", error);
-          }
-      },[courseAPI]);
-
-    useEffect(() => {
-        fetchData();
-      }, [fetchData]);
-    
-    return { fetchData };
-}
-
+  return { fetchData };
+};
 
 const useAxiosFetchApprentissage = () => {
-
   const { userProgressionAPI } = useAPIContext();
 
-  const fetchDataApprentissage = useCallback(async (value) => {
+  const fetchDataApprentissage = useCallback(
+    async (value) => {
       try {
-            if( value !== undefined) {
-              const response = await userProgressionAPI.searchProgressionStudent(value);
-              return response;
-            }
-
-        } catch (error) {
-          console.error("Error fetching apprentissage data:", error);
+        if (value !== undefined) {
+          const response = await userProgressionAPI.searchProgressionStudent(
+            value
+          );
+          return response;
         }
-    },[userProgressionAPI]);
+      } catch (error) {
+        console.error("Error fetching apprentissage data:", error);
+      }
+    },
+    [userProgressionAPI]
+  );
 
   useEffect(() => {
     fetchDataApprentissage();
-    }, [fetchDataApprentissage]);
-  
-  return { fetchDataApprentissage };
-}
+  }, [fetchDataApprentissage]);
 
+  return { fetchDataApprentissage };
+};
 
 const checkEmptyValue = (obj) => {
   for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && obj[key] !== '') {
+    if (Object.prototype.hasOwnProperty.call(obj, key) && obj[key] !== "") {
       return true;
     }
   }
   return false;
-}
+};
 
-
-export { checkEmptyValue , useAxiosFetchCourse , useAxiosFetchApprentissage};
+export { checkEmptyValue, useAxiosFetchCourse, useAxiosFetchApprentissage };
